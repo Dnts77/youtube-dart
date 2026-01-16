e83b41a75fd8c9425be553dbd6ca3891a81f14e1
Dnts77 <daniel.souzadantas07@gmail.com>
2026-01-16 18:45:07 -0300

    feat: Método de listagem de vídeos & retorno com FutureBuilder e ListView

--- lib/screens/inicio.dart
+++ lib/screens/inicio.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube/api.dart';
+import 'package:youtube/model/video.dart';
 
 
 class Inicio extends StatefulWidget {
@@ -10,23 +11,77 @@ class Inicio extends StatefulWidget {
 }
 
 class _InicioState extends State<Inicio> {
+
+  _listVideo(){
+    Future<List<Video>> videos;
+    
+    Api api = Api();
+    
+    
+    return api.pesquisar("");
+  }
+
   @override
   Widget build(BuildContext context) {
 
 
-    Api api = Api();
-    api.pesquisar("");
     
-    return Container(
-        padding: EdgeInsets.all(12),
-        child: Center(
-          child: Text(
-            "Início",
-            style: TextStyle(
-              fontSize: 25
-            ),
-            ),
-        ),
+    
+    return FutureBuilder<List<Video>>(
+      future: _listVideo(), 
+      builder: (context, snapshot){
+        switch(snapshot.connectionState){
+          case ConnectionState.none:
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+            
+          case ConnectionState.done:
+            if(snapshot.hasData){
+              return ListView.separated(
+                itemBuilder: (context, index){
+
+                  List<Video> videos = snapshot.data!;
+                  Video video = videos[index];
+
+                  return Column(
+                    children: [
+                      Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(video.imagem!),
+                            fit: BoxFit.cover
+                            )
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(video.titulo!),
+                        subtitle: Text(video.canal!),
+                      )
+                    ],
+                  );
+                }, 
+                separatorBuilder: (context, index) => Divider(
+                  height: 2,
+                  color: Colors.grey,
+                ), 
+                itemCount: snapshot.data!.length
+                );
+            }
+            else{
+              Center(
+              child: Text("Nenhum dado a ser exibido."),
+            );
+            }
+
+            break;
+
+        }
+        throw '';
+      }
       );
     
   }
