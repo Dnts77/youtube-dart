fabf88b9c90f7382a83304e70162d09e6e053c8e
Dnts77 <daniel.souzadantas07@gmail.com>
2026-01-16 16:08:50 -0300

    feat: conversão de json para o modelo de Video

--- lib/model/video.dart
+++ lib/model/video.dart
@@ -8,4 +8,25 @@ class Video {
 
   Video({this.canal, this.descricao, this.id, this.imagem, this.titulo});
 
+  /*static jsonConvert(Map<String, dynamic> json){
+    return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      imagem: json["snippet"]["thumbnails"]["high"]["url"],
+      descricao: json["snippet"]["description"],
+      canal: json["snippet"]["channelId"]
+    );
+  } */
+  
+  
+  factory Video.fromJson(Map<String, dynamic> json){
+   return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      imagem: json["snippet"]["thumbnails"]["high"]["url"],
+      descricao: json["snippet"]["description"],
+      canal: json["snippet"]["channelId"]
+    );
+  }
+
 }
